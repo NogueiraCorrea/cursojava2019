62942dd3fd8420d3ac7a5df2799a498933eec06a
Aluno <Aluno@ALU24111.educa.sf>
2020-02-12 22:29:50 -0200

    Projetos

---
+++ Projeto03/src/Trator/Teclado.java
@@ -0,0 +1,28 @@
+package Trator;
+
+public class Teclado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		import util.Trator Teclado;
+	}
+		public static void main(String[] args) {
+		// Leitura de texto
+		String ano;
+		ano = Teclado.lerTexto("informe o ano: ");
+		// Leitura de inteiro
+		int ano;
+		quilometragem = Teclado.lerInt("informe a quilometragem: ");
+		//Leitura de Ponto Flutuante
+		double Trator;
+		ano = Teclado.lerDouble("Digite ano": );
+		System.out.println("ano informado" :  + ano);
+		System.out.println("quilometragem informada : " + quilometragem);
+		
+		}
+		
+		
+		
+	}
+
